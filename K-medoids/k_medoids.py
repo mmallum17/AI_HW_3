c2c6e3082697f8f6e9c4d7c19964d40545beae8e
Marcus Mallum <marcusmallum@gmail.com>
2018-11-12 14:03:49 -0600

    complete

--- K-medoids/k_medoids.py
+++ K-medoids/k_medoids.py
@@ -1,5 +1,6 @@
 import csv
 import math
+import random
 
 
 def read_csv_file():
@@ -29,7 +30,6 @@ def get_euclidean_dist(data_one, data_two, data_length):
         data_dim_two = data_two[i]
         distance = distance + (data_dim_one - data_dim_two)**2
 
-    # print(distance)
     distance = math.sqrt(float(distance))
 
     return distance
@@ -47,6 +47,29 @@ def calc_obj_dists(dist_sum):
         dist_list.append(v_dist)
 
     return dist_list
+
+
+def get_cluster_labels():
+    results = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+    i = 0
+    for c in clusters:
+        index_one = i
+        for data in c:
+            index_two = iris_dict[data[4]]
+            results[index_one][index_two] += 1
+        i += 1
+
+    labels = [0, 0, 0]
+    labels[0] = results[0].index(max(results[0]))
+    for i in range(len(results)):
+        results[i][labels[0]] = 0
+    labels[1] = results[1].index(max(results[1]))
+    for i in range(len(results)):
+        results[i][labels[1]] = 0
+    labels[2] = results[2].index(max(results[2]))
+
+    return labels
 
 
 def assign_data_to_medoids(k):
@@ -111,8 +134,21 @@ def find_new_medoid(m):
             min_dist = new_dist
             min_medoid = test_medoid
 
-
     return min_medoid
+
+
+def get_k_means_results(labels):
+    results = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+    i = 0
+    for c in clusters:
+        index_one = labels[i]
+        for data in c:
+            index_two = iris_dict[data[4]]
+            results[index_one][index_two] += 1
+        i += 1
+
+    return results
 
 
 def get_results():
@@ -137,6 +173,68 @@ def display_results(results):
         print(row_format.format(label, *row))
 
 
+def get_new_seed_points():
+    new_seed_points = []
+
+    # print(clusters)
+    for c in clusters:
+        new_seed_points.append(get_mean_point(c))
+
+    return new_seed_points
+
+
+def get_mean_point(c):
+    average = []
+    data_length = 0
+
+    for i in range(4):
+        average.append(0.0)
+
+    for data in c:
+        for i in range(4):
+            average[i] += data[i]
+
+    average_tuple = ()
+    for i in range(4):
+        average[i] = average[i] / len(c)
+        average_tuple = average_tuple + (average[i],)
+
+    return average_tuple
+
+
+def init_seed_points(k):
+    seed_points = random.sample(iris_data, k)
+    return seed_points
+
+
+def assign_data_to_clusters(k):
+    temp_clusters = []
+    for i in range(k):
+        temp_clusters.append(set())
+
+    for data in iris_data:
+        min_cluster = find_nearest_seed_point(data)
+        temp_clusters[min_cluster].add(data)
+
+    return temp_clusters
+
+
+def find_nearest_seed_point(data):
+    i = 0
+    min_dist = float("inf")
+    min_cluster = 0
+
+    # print(seed_points)
+    for sp in seed_points:
+        dist = get_euclidean_dist(data, sp, 4)
+        if dist < min_dist:
+            min_dist = dist
+            min_cluster = i
+        i += 1
+
+    return min_cluster
+
+
 iris_dict = {
     'Iris-virginica': 0,
     'Iris-versicolor': 1,
@@ -144,6 +242,26 @@ iris_dict = {
 }
 
 iris_data = read_csv_file()
+
+# K-Means
+clusters = set()
+seed_points = init_seed_points(3)
+iterations = 0
+while True:
+    iterations += 1
+    new_clusters = assign_data_to_clusters(3)
+    if new_clusters == clusters:
+        break
+    clusters = new_clusters
+    seed_points = get_new_seed_points()
+
+labels = get_cluster_labels()
+results = get_k_means_results(labels)
+print("K-MEANS")
+display_results(results)
+print()
+
+# K-Medoid
 dist_sum = calc_dist_sum()
 dist_list = calc_obj_dists(dist_sum)
 length = len(dist_list)
@@ -167,4 +285,5 @@ while True:
     obj_medoid_dist_sum = new_obj_medoid_dist_sum
 
 results = get_results()
+print("K-MEDOIDS")
 display_results(results)
